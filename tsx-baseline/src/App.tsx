1b1a44989ad600ec5f9c834c201742f9c6485127
milky <listingslab@gmail.com>
2022-06-29 12:27:12 +0200

    cleans App

--- tsx-baseline/src/App.tsx
+++ tsx-baseline/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+
+        <p>
+          add a button here
+        </p>
+
+        <p>
+          and here
+        </p>
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>

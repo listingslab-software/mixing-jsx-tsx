3a7d25e8af9b947673839720ca15d8468f31ab82
milky <listingslab@gmail.com>
2022-06-29 20:55:25 +0200

    No problems here either

--- jsx-baseline/src/App.js
+++ jsx-baseline/src/App.js
@@ -1,58 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { 
+  Box,
+  Button,
+  Typography,
+} from "@mui/material"
+import MiniDrawerTsx from './TSX/MiniDrawerTsx.tsx'
+import MiniDrawerJsx from './JSX/MiniDrawerJsx'
 
 function App() {
+
+  const [mode, setMode] = React.useState('default')
+
+  const switchMode = (newMode: string) => {
+    setMode(newMode)
+  }
+
+  if(mode === "jsx"){
+    return <MiniDrawerJsx />
+  }
+
+
+  if(mode === "tsx"){
+    return <MiniDrawerTsx />
+  }
+
+
   return (
-    <div className="App">
+    <Box className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+
+          <Typography variant="h4" sx={{fontWeight: "lighter", mb:2}}>
+            POC/1
+          </Typography>
+
+          <Button
+            sx={{m:1}}
+            variant="contained"
+            color="primary"
+            onClick={ (e) => {
+              e.preventDefault()
+              switchMode('tsx')
+            }}>
+            TypeScript Version
+          </Button>
+
+          <Button
+            sx={{m:1}}
+            variant="contained"
+            color="secondary"
+            onClick={ (e) => {
+              e.preventDefault()
+              switchMode('jsx')
+            }}>
+           JSX (ES6) Version
+          </Button>
+
+        <Box sx={{ mt:2}}>
+          <Typography variant="body2">
+            Edit <code>src/App.tsx</code> and save to reload.
+          </Typography>
+          <Typography variant="body2">
+            <span>Learn </span>
+            <a
+              className="App-link"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux-toolkit.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux Toolkit
+            </a>
+            ,<span> and </span>
+            <a
+              className="App-link"
+              href="https://react-redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React Redux
+            </a>
+          </Typography>
+        </Box>
       </header>
-    </div>
-  );
+    </Box>
+  )
 }
 
-export default App;
+export default App
